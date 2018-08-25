d7c3aed41589991941513a53f13ddf948ffc6438
Maharshee Roy <m.roy@optonline.net>
2018-08-25 01:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED"
+  }
+  if (string === "I love you, grandma.")
+  return "I love you, too."
 }
